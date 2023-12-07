9c3c9d8eb5283e2566f101af5be60f61a455ff4f
Ana Škarica <ana.skarica.as@gmail.com>
2023-12-07 22:37:36 +0100

    Add dynamic table of users and jobs with mock data

--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "../../Components/Table/Table";
 
 const Dashboard = () => {
+  const [selectedOption, setSelectedOption] = useState("users");
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <div>
-      Dashboard
-      <Table />
+    <div className="max-w-7xl mx-auto mt-8">
+      <h2 className="font-bold text-2xl">
+        Popis {selectedOption == "users" ? "korisnika" : "poslova"}
+      </h2>
+      <div className="flex gap-4 mt-8">
+        <label
+          className={`cursor-pointer p-2 border border-yellow-dark rounded w-32 text-center ${
+            selectedOption === "users"
+              ? "bg-yellow-light text-white"
+              : "text-yellow-dark"
+          }`}
+        >
+          <input
+            type="radio"
+            value="users"
+            checked={selectedOption === "users"}
+            onChange={() => handleOptionChange("users")}
+            className="hidden"
+          />
+          Korisnici
+        </label>
+
+        <label
+          className={`cursor-pointer p-2 border border-yellow-light rounded w-32 text-center ${
+            selectedOption === "jobs"
+              ? "bg-yellow-light text-white"
+              : "text-yellow-light"
+          }`}
+        >
+          <input
+            type="radio"
+            value="jobs"
+            checked={selectedOption === "jobs"}
+            onChange={() => handleOptionChange("jobs")}
+            className="hidden"
+          />
+          Poslovi
+        </label>
+      </div>
+      <Table selectedOption={selectedOption} />
     </div>
   );
 };
