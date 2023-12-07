0d0c4e39f404e6cb59ea45c93996aecf07429788
robert4361 <robert4361@gmail.com>
2023-12-07 21:16:03 +0100

    Add endpoint for signup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,14 +4,24 @@ const service = require("../services/usersService");
 async function login(req, res) {
   const {username, password} = req.body
   const response = await service.login(username, password);
-  if(response.error != undefined) {
+  if (response.error != undefined) {
     res.status(401).json(response)
+  } else {
+    res.json(response);
   }
-  else {
+}
+
+async function signup(req, res) {
+  const {username, password, admin} = req.body;
+  const response = await service.signup(username, password, admin);
+  if (response.error != undefined) {
+    res.status(401).json(response)
+  } else {
     res.json(response);
   }
 }
 
 module.exports = {
-  login
+  login,
+  signup
 }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const controller = require("../controllers/authController")
 
-router.post('/login', controller.login)
+router.post('/login', controller.login);
+
+router.post('/signup', controller.signup);
 
 module.exports = router;
